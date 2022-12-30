0d6aa2e66ab96a13491c98ab51adc918756d3950
Nino-gb <110786136+Nino-gb@users.noreply.github.com>
2022-12-30 09:31:40 -0600

    Delete config.py

--- Data_Collection/config.py
+++
@@ -1,8 +0,0 @@
-# API Sites Keys
-
-kopenweather = "f2d24a548c3fa1b72a362ab158d35a2f" # Open Weather
-kgmaps = "AIzaSyC-t0H6biJFdq5YsCGBXk4ixr39J46y5RA" # Google maps
-knyt = "inHGdGmSABEiCgA017faTtFPriPT6Li1" # New York Times
-ksql = "Conejo1818?" # MY SQL database
-kmapbox = "pk.eyJ1IjoiY29uZWpvcGVyZXoxNiIsImEiOiJjbDl3YTU4MnEweXQ2M3VxeWJhM292NTEyIn0.qD8KHA5BYHhtS-Fr7PhY0A" # Mapbox
-krapid = "e5438e6cd9msh8aec3004b5897ddp1bc47fjsnf25e52105149" #Rapid API
